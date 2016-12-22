d0578b7de0639f02234bde8afe09bda19dc27f90
Daniel <DanielParkinson45@gmail.com>
2016-12-22 19:35:13 +1000

    Assets - Images
    The game only has 1 sound file at the moment, so the assets are all images or json files for the tilemaps

---
+++ Assets/GroundTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="GroundTiles" tilewidth="32" tileheight="32" tilecount="18" columns="18">
+ <image source="../../../Assets_In_Progress/GroundTiles.png" trans="ff00ff" width="576" height="32"/>
+</tileset>
---
+++ Assets/MovingPlatform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MovingPlatform" tilewidth="64" tileheight="32" tilecount="4" columns="4">
+ <image source="MovingPlatform.png" trans="ff00ff" width="256" height="32"/>
+</tileset>
